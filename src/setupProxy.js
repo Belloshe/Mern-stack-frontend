23acfafcc6362f66b4e790648170644519dcd004
Belloshe <isabelle.ibrahimsson@edu.cmeducations.se>
2024-06-11 21:36:16 +0200

    changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://mern-stack-zhce.onrender.com',
       changeOrigin: true,
     })
   );
